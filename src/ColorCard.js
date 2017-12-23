f4cd56c02580feb7463226ee38868f126317319b
daiglerj <daiglerj@canisius.edu>
2017-12-23 12:31:43 -0500

    fixed positioning issues

--- src/ColorCard.js
+++ src/ColorCard.js
@@ -41,6 +41,7 @@ export class Card extends React.Component {
               height: 200,
               width: 150,
               padding: 0,
+              marginBottom:20,
               backgroundColor: "#FFF",
               WebkitFilter: "drop-shadow(0px 0px 5px #666)",
               filter: "drop-shadow(0px 0px 5px #666)"
